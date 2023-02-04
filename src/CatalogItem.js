1cd844314e859241065716fbfa833a1730b8a1ce
valentinazhbanova <112694404+valentinazhbanova@users.noreply.github.com>
2023-02-04 11:55:05 +0300

    Update CatalogItem.js

--- src/CatalogItem.js
+++ src/CatalogItem.js
@@ -2,7 +2,7 @@ function CatalogItem({buyDishes}) {
     return(
         <div className="parents">
             {buyDishes.map((element => {
-            const {id, name, searchTerm, price, image} = element;
+            const {id, name, price, image} = element;
             return(
                 <div key={id}>
                     <div  className='parentsItem'>
